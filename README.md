7110fabf355f952f03a128e6f31579ab028dce8c
Michael Becker <michael.f.becker@gmail.com>
2016-09-04 16:36:04 -0400

    Adding command line options
    This allows the simulator to be configured at run time.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # monty-hall-problem
 Test various combinations of the Monty Hall Problem.
+
+## Usage
+```
+usage: monty.py [-h] [--simple-game] [--monty-pick-random]
+                [--monty-pick-first] [--contestant-picks-same]
+                [--contestant-picks-swap] [--contestant-picks-random]
+                [--number-runs NUMBER_RUNS]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --simple-game         Run a simple game, no swapping at all.
+  --monty-pick-random   [Default] Monty picks a random goat if possible.
+  --monty-pick-first    Monty picks the first goat possible.
+  --contestant-picks-same
+                        The contestant keeps the first door they picked.
+  --contestant-picks-swap
+                        [Default] The contestant swaps the door they picked
+                        with the other one.
+  --contestant-picks-random
+                        The contestant "randomly" selects between the two
+                        doors left.
+  --number-runs NUMBER_RUNS
+                        How many times to simulate the game. Default = 10,000
+```
+
--- monty.py
+++ monty.py
@@ -33,15 +33,12 @@ Test various combinations of the Monty Hall Problem.
 """
 import random
 import sys
+import argparse
 
-# Do this at the start, we are going to use this module a lot.
-random.seed();
-
-total_games = 100000
-games_run = 0
-total_wins = 0
-
-
+# The stage is what we work from. 
+#  0 => goat
+#  1 => car
+# -1 => Monty opened the door and showed us the goat.
 def create_stage():
     """Create the initial game"""
     car_index = random.randrange(3)
@@ -70,13 +67,10 @@ def monty_pick_first_possible(stage, contestant_pick):
     """Monty picks the first possbile goat."""
     if stage[0] == 0 and contestant_pick != 0:
         stage[0] = -1
-        return 0
     elif stage[1] == 0 and contestant_pick != 1:
         stage[1] = -1
-        return 1
     else:
         stage[2] = -1
-        return 2
 
 
 def monty_pick_random(stage, contestant_pick):
@@ -90,10 +84,9 @@ def monty_pick_random(stage, contestant_pick):
         else:
             break
     stage[pick] = -1
-    return pick
 
 
-def contestant_pick_again_random(adjusted_stage):
+def contestant_pick_again_random(adjusted_stage, contestant_first_pick):
     """The contestant picks randomly fron the remaining choices."""
     pick = random.randrange(3)
     while adjusted_stage[pick] == -1:
@@ -101,19 +94,19 @@ def contestant_pick_again_random(adjusted_stage):
     return pick;
 
 
-def contestant_pick_again_same(contestant_pick):
+def contestant_pick_again_same(adjusted_stage, contestant_first_pick):
     """Degenerate case, contestant stays with their initial pick."""
-    return contestant_pick
+    return contestant_first_pick
 
 
-def contestant_pick_again_swap(adjusted_stage, pick):
+def contestant_pick_again_swap(adjusted_stage, contestant_first_pick):
     """Contestant swaps choices with the remaining door."""
-    if pick == 0:
+    if contestant_first_pick == 0:
         if adjusted_stage[1] == -1:
             return 2
         else:
             return 1
-    elif pick == 1:
+    elif contestant_first_pick == 1:
         if adjusted_stage[0] == -1:
             return 2
         else:
@@ -125,21 +118,73 @@ def contestant_pick_again_swap(adjusted_stage, pick):
             return 0
 
 
-def run_game():
+def run_normal_game():
     """Run a complex game, like the real thing."""
     stage = create_stage()
     contestant_first_pick = random.randrange(3)
 
-    #monty_pick = monty_pick_first_possible(stage, contestant_first_pick)
-    monty_pick = monty_pick_random(stage, contestant_first_pick)
+    montys_pick_algorithm(stage, contestant_first_pick)
 
-    contestant_second_pick = contestant_pick_again_random(stage)
-    #contestant_second_pick = contestant_pick_again_same(contestant_first_pick)
-    #contestant_second_pick = contestant_pick_again_swap(stage, contestant_first_pick)
+    contestant_second_pick = contestants_second_pick_algorithm(stage, contestant_first_pick)
 
     wins = contestant_wins(stage, contestant_second_pick)
     #print (stage, contestant_first_pick, contestant_second_pick, wins)
     return wins
+
+
+# Lets take some arguments so we can experiment without changing code.
+parser = argparse.ArgumentParser()
+parser.add_argument("--simple-game", action='store_true', 
+        help="Run a simple game, no swapping at all.")
+parser.add_argument("--monty-pick-random", action='store_true', 
+        help="[Default] Monty picks a random goat if possible.")
+parser.add_argument("--monty-pick-first", action='store_true', 
+        help="Monty picks the first goat possible.")
+parser.add_argument("--contestant-picks-same", action='store_true', 
+        help="The contestant keeps the first door they picked.")
+parser.add_argument("--contestant-picks-swap", action='store_true', 
+        help="[Default] The contestant swaps the door they picked with the other one.")
+parser.add_argument("--contestant-picks-random", action='store_true', 
+        help="The contestant \"randomly\" selects between the two doors left.")
+parser.add_argument("--number-runs", type=int, 
+        help="How many times to simulate the game. Default = 10,000")
+args = parser.parse_args()
+
+
+# Do this at the start, we are going to use this module a lot.
+random.seed();
+
+total_games = 10000
+games_run = 0
+total_wins = 0
+
+# How many test runs?
+if args.number_runs:
+    total_games = args.number_runs
+
+if args.simple_game:
+    # If its a "simple" game, we don't use any other algorithms.
+    run_game = run_simple_game
+else:
+    run_game = run_normal_game
+
+    # How will Monty pick a goat to show us?
+    if args.monty_pick_random:
+        montys_pick_algorithm = monty_pick_random
+    elif args.monty_pick_first:
+        montys_pick_algorithm = monty_pick_first_possible
+    else:
+        montys_pick_algorithm = monty_pick_random
+
+    # How will the contestant pick when presented with a second choice?
+    if args.contestant_picks_same:
+        contestants_second_pick_algorithm = contestant_pick_again_same
+    elif args.contestant_picks_swap:
+        contestants_second_pick_algorithm = contestant_pick_again_swap
+    elif args.contestant_picks_random:
+        contestants_second_pick_algorithm = contestant_pick_again_random
+    else:
+        contestants_second_pick_algorithm = contestant_pick_again_swap
 
 
 # Simulate many games and count the winners.
@@ -148,7 +193,13 @@ while games_run != total_games:
     if run_game():
         total_wins = total_wins + 1
 
+
 # And let us know how we did.
+print ("Game Type:", run_game.__name__)
+print ("Monty's Selection Algorithm:", contestants_second_pick_algorithm.__name__)
+print ("Contestant's Second Pick Algorithm:", montys_pick_algorithm.__name__)
+print ("Total Games:", total_games)
+print ("Games Won:", total_wins)
 print ("Odds of winning:", total_wins * 100.0 / total_games, "%")
 
 
